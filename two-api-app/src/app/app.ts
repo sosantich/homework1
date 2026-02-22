6286d732fead29b8d61c3839608c093bb043bdca
German <utubcirik@gmail.com>
2026-02-22 11:01:32 +0300

    fix app.ts

--- two-api-app/src/app/app.ts
+++ two-api-app/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet,  } from '@angular/router';
-import { ItemsTableComponent } from './components/items-table/items-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ItemsTableComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
